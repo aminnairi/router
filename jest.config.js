5c58995f5ed65639e7627f235423ed04ab00b6ea
Amin NAIRI <nairi.amin@gmail.com>
2020-05-10 23:05:30 +0200

    :rotating_light: fixed linter issues

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,13 @@
-"use strict";
-
 module.exports = {
     collectCoverage: true,
     moduleFileExtensions: [
         "mjs",
         "js"
     ],
-    transform: {
-        "\.mjs$": "babel-jest"
-    },
     testMatch: [
         "<rootDir>/tests/*.mjs"
     ],
-    maxConcurrency: 1
+    transform: {
+        "\\.mjs$": "babel-jest"
+    }
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,26 @@
 import remove from "rollup-plugin-delete";
-
-import {resolve, extname} from "path";
 import {terser} from "rollup-plugin-terser";
+import {extname, resolve} from "path";
 
-function bundle(input, output, empty) {
+const bundle = (input, output, empty) => {
     const extension = extname(output);
 
     return {
         input: resolve("sources", input),
 
+        output: {
+            file: ".mjs" === extension ? resolve("release", "module", input) : ".cjs" === extension ? resolve("release", "commonjs", output) : resolve("release", "browser", output),
+            format: ".mjs" === extension ? "esm" : ".cjs" === extension ? "cjs" : "iife",
+            name: "Router"
+        },
+
         plugins: [
             empty && remove({targets: resolve("release", "*")}),
             terser()
-        ],
+        ]
 
-        output: {
-            file: extension === ".mjs" ? resolve("release", "module", input) : extension === ".cjs" ? resolve("release", "commonjs", output) : resolve("release", "browser", output),
-            format: extension === ".mjs" ? "esm" : extension === ".cjs" ? "cjs" : "iife",
-            name: "Router"
-        }
     };
-}
+};
 
 export default [
     bundle("router.mjs", "router.mjs", true),
